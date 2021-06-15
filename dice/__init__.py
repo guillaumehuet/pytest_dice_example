4e0527b23e37b0f2c92d30d51abecb6bd51ebd3b
Guillaume HUET <guillaume.gh.huet@gmail.com>
2021-06-15 15:25:41 +0200

    Added RNG input to dice class

--- dice/__init__.py
+++ dice/__init__.py
@@ -1,9 +1,10 @@
 import random
 
 class Dice():
-  def __init__(self, value=None):
+  def __init__(self, value=None, rng=None):
+    self.rng = rng or random.Random()
     if value is None:
-      self.value = random.randint(1, 6)
+      self.value = self.rng.randint(1, 6)
     else:
       if 1 <= value  <= 6:
         self.value = value
@@ -11,7 +12,7 @@ class Dice():
         raise ValueError("Can only initialize a dice with a value between 1 and 6.")
   
   def roll(self):
-    self.value = random.randint(1, 6)
+    self.value = self.rng.randint(1, 6)
 
   def __repr__(self):
     return str(self.value)
